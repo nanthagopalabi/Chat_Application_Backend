15bd98cdb0c58728367e1656dcd81e3d68a6c106
Nanthagopal <welcum2nantha@gmail.com>
2024-02-19 22:41:43 +0530

    mongoose version chage

--- Middleware/authenticate.js
+++ Middleware/authenticate.js
@@ -1,6 +1,7 @@
 import  jwt from "jsonwebtoken";
-// import { User } from "../Model/userModel.js";
 import { User } from "../Model/userModel.js";
+
+
 
 function getUserById(id) {
     return User.findById(id).select("_id name email");;
