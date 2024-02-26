3960ba87a7da200d9bfda1cfff3afe6838388050
Nanthagopal <welcum2nantha@gmail.com>
2024-02-26 18:27:26 +0530

    changes in route path

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,21 +1,12 @@
 import express from 'express'
 import { allMessage, sendMsg } from '../controllers/messageController.js';
 
-
 const router=express.Router();
 
 //Post personal Message
-router.route("/sendMsg").post(sendMsg);
+router.route("/").post(sendMsg);
 
 //get all messages
 router.route("/:chatId").get(allMessage);
-
-// //to create a group chat
-// router.route("/group").post(createGroupChat);
-
-// router.route("/rename").put(groupRenaming)
-// router.route("/groupremove").put(removeUserFromGroup);
-// router.route("/groupadd").put(addFromGroup);
-
 
 export {router as msgRouter}
